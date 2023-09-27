1f663976c7676db4abb3d9bf7b3d5e99afd84e83
nixberg <310825+nixberg@users.noreply.github.com>
2023-09-27 23:46:17 +0200

    Update TupleSequencesTests.swift

--- Tests/TupleSequencesTests/TupleSequencesTests.swift
+++ Tests/TupleSequencesTests/TupleSequencesTests.swift
@@ -7,105 +7,58 @@ final class TupleSequencesTests: XCTestCase {
     
     func testCouples() {
         let sequence = array.couples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 2)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 2), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 2), by: ==))
     }
     
     func testTriples() {
         let sequence = array.triples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 3)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 3), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1] &&
-            $0.2 == $1[offsetBy: 2]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 3), by: ==))
     }
     
     func testQuadruples() {
         let sequence = array.quadruples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 4)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 4), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1] &&
-            $0.2 == $1[offsetBy: 2] &&
-            $0.3 == $1[offsetBy: 3]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 4), by: ==))
     }
     
     func testQuintuples() {
         let sequence = array.quintuples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 5)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 5), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1] &&
-            $0.2 == $1[offsetBy: 2] &&
-            $0.3 == $1[offsetBy: 3] &&
-            $0.4 == $1[offsetBy: 4]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 5), by: ==))
     }
     
     func testSextuples() {
         let sequence = array.sextuples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 6)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 6), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1] &&
-            $0.2 == $1[offsetBy: 2] &&
-            $0.3 == $1[offsetBy: 3] &&
-            $0.4 == $1[offsetBy: 4] &&
-            $0.5 == $1[offsetBy: 5]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 6), by: ==))
     }
     
     func testSeptuples() {
         let sequence = array.septuples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 7)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 7), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1] &&
-            $0.2 == $1[offsetBy: 2] &&
-            $0.3 == $1[offsetBy: 3] &&
-            $0.4 == $1[offsetBy: 4] &&
-            $0.5 == $1[offsetBy: 5] &&
-            $0.6 == $1[offsetBy: 6]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 7), by: ==))
     }
     
     func testOctuples() {
         let sequence = array.octuples()
-        
         XCTAssertEqual(sequence.underestimatedCount, array.underestimatedCount / 8)
-        
-        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 8), by: {
-            $0.0 == $1[offsetBy: 0] &&
-            $0.1 == $1[offsetBy: 1] &&
-            $0.2 == $1[offsetBy: 2] &&
-            $0.3 == $1[offsetBy: 3] &&
-            $0.4 == $1[offsetBy: 4] &&
-            $0.5 == $1[offsetBy: 5] &&
-            $0.6 == $1[offsetBy: 6] &&
-            $0.7 == $1[offsetBy: 7]
-        }))
+        XCTAssert(sequence.elementsEqual(array.chunks(ofCount: 8), by: ==))
     }
 }
 
-extension RandomAccessCollection {
-    fileprivate subscript(offsetBy distance: Int) -> Element {
-        self[self.index(startIndex, offsetBy: distance)]
+// TODO: Use variadic generics when possible.
+extension RandomAccessCollection where Element: Equatable {
+    fileprivate static func == <T>(lhs: T, rhs: Self) -> Bool {
+        assert(_isPOD(T.self))
+        assert(MemoryLayout<Element>.size == MemoryLayout<Element>.stride)
+        assert(MemoryLayout<T>.size == MemoryLayout<Element>.stride * rhs.count)
+        return withUnsafeBytes(of: lhs) {
+            UnsafeBufferPointer(
+                start: $0.baseAddress!.assumingMemoryBound(to: Element.self),
+                count: rhs.count
+            ).elementsEqual(rhs)
+        }
     }
 }

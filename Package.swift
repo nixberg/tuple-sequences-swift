e43f7c466f2d8d3c08f468505e31e30588fea4eb
nixberg <310825+nixberg@users.noreply.github.com>
2024-09-14 21:21:54 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -10,7 +10,9 @@ let package = Package(
             targets: ["TupleSequences"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-algorithms", from: "1.0.0"),
+        .package(
+            url: "https://github.com/apple/swift-algorithms",
+            from: "1.0.0"),
     ],
     targets: [
         .target(
